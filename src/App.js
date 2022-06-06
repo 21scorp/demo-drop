5996c629da17041ed52d74c08227b83bb9e3673f
21scorp <quincyvana@gmail.com>
2022-06-06 16:39:42 +0200

    Homepagina af

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-background">
+
+        <p id="welcome">
+          Welcome to DJ Don Diablo - Promo Delivery System
+        </p>
+      </header>
+
+        <body>
+        <div className="App-header">
+          <nav>
+            <ul id="mainMenu">
+              <li><a href="">Login</a></li>
+              <li><a href="">Registratie</a></li>
+              <li><a href="">Profiel</a></li>
+              <li><a href="">Demo-drop</a></li>
+            </ul>
+          </nav>
+        </div>
+        </body>
+    </div>
+  );
+}
+
+export default App;
