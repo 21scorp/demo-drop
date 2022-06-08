becadad490b3bcbaace5567565f15fb8bb78e8cd
21scorp <quincyvana@gmail.com>
2022-06-08 16:55:38 +0200

    Registratie pagina design af

--- src/routes/Registratie.js
+++ src/routes/Registratie.js
@@ -1,8 +1,24 @@
-
+import './Registratie.css'
 export default function Registratie() { document.getElementById("welcome").style.display = "none";
     return (
         <main>
             <h2>Registratie</h2>
+            <div className="vierkantbox2">
+                <div className="vierkant2">
+                    <form>
+                        <label>
+                            <input id="gebruikersnaam2" type="text" name="Gebruikersnaam" placeholder="Gebruikersnaam" />
+                        </label>
+                        <label>
+                            <input id="email" type="text" name="Email" placeholder="Email" />
+                        </label>
+                        <label>
+                            <input id="wachtwoord2" type="password" name="Wachtwoord" placeholder="Wachtwoord" />
+                        </label>
+                        <input id="button2" type="submit" value="Login" />
+                    </form>
+                </div>
+            </div>
         </main>
     );
 }
